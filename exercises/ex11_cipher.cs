a965f3308fb62f13935987134a6fc39c89c0681f
Nick Terry <nick.terry1@outlook.com>
2018-04-25 15:58:52 -0400

    initial commit

---
+++ exercises/ex11_cipher.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// author: nick terry
+// date: apr 25, 2018
+
+// this program  has three parts. Part one creates a cipher that takes a user input string, converts it to unicode, adds a constant
+// number and converts it back to ascii. The second part takes that input adds values that are represented as a word and returns the
+// ascii value. The third part uses the sample word and itself as the unicode to add to itself and return the resulting ascii values. 
+// I wanted to use all unicode characters in the code but the instructions were to only use uppercase letters.
+
+namespace ex10_cipher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // these are all to sample the program using all three methods
+            Console.WriteLine("Please insert a string that consists of upper letters only");
+            string input = Console.ReadLine();
+            Console.WriteLine(Encrypt1(input));
+            Console.WriteLine(Encrypt2(input));
+            Console.WriteLine(Encrypt3(input));
+            Console.ReadLine();
+        }
+
+        public static string Encrypt1(string input)
+        {
+            // this method is just in case the user doesnt follow the initial instructions
+            input = input.ToUpper();
+            StringBuilder result = new StringBuilder();
+            foreach (char letter in input)
+            {
+                // converting each char into its unicode and adding 3 to it
+                int temp = (int)letter;
+                temp += 3;
+
+                // The characters need to wrap and the last capitol letter 'Z' is unicode 90
+                if (temp > 90)
+                {
+                    temp -= 26;
+                }
+
+                // converting the unicode back to char
+                result.Insert(result.Length, (char)temp);
+            }            
+            return result.ToString();
+        }
+
+        public static string Encrypt2(string input)
+        {
+            int index = input.Length;
+
+            // using the unicode values for "CAT" instead of three
+            string cipher = "CAT";
+            input = input.ToUpper();
+            StringBuilder result = new StringBuilder();
+            for (int i = 0; i < input.Length; i++)
+            {
+                int temp = (int)input[i];
+
+                // modulating i by the cipher length to make sure it stays within the bounds of the cipher string
+                temp += (int)cipher[i % cipher.Length] - 60;
+                if (temp > 90)
+                {
+                    temp -= 26;
+                }
+                result.Insert(result.Length, (char)temp);
+            }
+            return result.ToString();
+        }
+
+        public static string Encrypt3(string input)
+        {
+            int index = input.Length;
+            input = input.ToUpper();
+
+            // concatinating the input with hardcoded "CAT". The only change from the second encryption method
+            string cipher = "CAT" + input;
+            StringBuilder result = new StringBuilder();
+            for (int i = 0; i < input.Length; i++)
+            {
+                int temp = (int)input[i];
+                temp += (int)cipher[i % cipher.Length] - 60;
+                if (temp > 90)
+                {
+                    temp -= 26;
+                }
+                result.Insert(result.Length, (char)temp);
+            }
+            return result.ToString();
+        }
+    }
+}
---
+++ exercises/ex9_encryption.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// author: nick terry
+// date: apr 25, 2018
+
+// This program allows the user to create an account in local memory, authenticate a user, and exit. Upon exitting 
+// all the username & password combinations are printed to the screen. 
+
+namespace ex9_encryption
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {            
+            // user stores all the username passwords
+            Dictionary<string, string> user = new Dictionary<string, string>();
+
+            MENU:
+            try
+            {
+                // all routes return to the main menu
+                Console.Clear();
+                Console.WriteLine("" +
+                    "----------------------------------------------------------------\n" +
+                    "-                         Pick an option                       -\n" +
+                    "-                                                              -\n" +
+                    "-      1) Establish an account                                 -\n" +
+                    "-      2) Authenticate a user                                  -\n" +
+                    "-      3) Exit the system                                      -\n" +
+                    "-                                                              -\n" +
+                    "-                                                              -\n" +
+                    "----------------------------------------------------------------\n");
+                int input = int.Parse(Console.ReadLine());
+                switch (input)
+                {
+                    case 1:
+                        MakeAccount(ref user);
+                        goto MENU;
+                    case 2:
+                        Authenticate(user);
+                        goto MENU;
+                    case 3:
+                        // printing each username and password to the screen then exiting
+                        foreach (KeyValuePair<string, string> k in user)
+                        {
+                            Console.WriteLine(k);
+                        }
+                        Console.ReadLine();
+                        return;
+                    default:
+                        Console.WriteLine("Thats not an option. Pick a number 1-3 next time");
+                        Console.ReadLine();
+                        goto MENU;
+                }
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("You can only input integer numbers. Try again");
+                Console.ReadLine();
+                goto MENU;
+            }
+            catch (Exception Ex)
+            {
+                Console.WriteLine(Ex.Message);
+                Console.ReadLine();
+                goto MENU;
+            }
+        }
+
+        public static string Encrypt(string input)
+        {
+            StringBuilder result = new StringBuilder();
+
+            // passKey contains all the characters that are acceptable for a password
+            char[] passKey = new char[] { 'a', 'b', 'c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',
+            'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',
+            '0','1','2','3','4','5','6','7','8','9' };
+
+            // passVal is the parrallel array that changes the value of the passKey into
+            char[] passVal = new char[] { 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+            'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z','0','1','2','3','4','5','6','7','8','9',
+            'a', 'b', 'c','d','e','f','g','h','i','j','k','l','m','A','B','C','D','E','F','G','H','I','J','K','L','M'};
+
+            // adding the corresponding result one char at a time to the final result
+            foreach (char letter in input)
+            {
+                int index = Array.IndexOf(passKey, letter);
+                result.Append(passVal[index]);
+            }
+
+            return result.ToString();
+        }
+
+        // decrypt not used in this program
+        public static string Decrypt(string input)
+        {
+            StringBuilder result = new StringBuilder();
+            char[] passKey = new char[] { 'a', 'b', 'c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',
+            'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',
+            '0','1','2','3','4','5','6','7','8','9' };
+            char[] passVal = new char[] { 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+            'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z','0','1','2','3','4','5','6','7','8','9',
+            'a', 'b', 'c','d','e','f','g','h','i','j','k','l','m','A','B','C','D','E','F','G','H','I','J','K','L','M'};
+
+            // Adding the corresponding password value to the final result one char at a time
+            foreach (char letter in input)
+            {
+                int index = Array.IndexOf(passVal, letter);
+                result.Append(passKey[index]);
+            }
+
+            return result.ToString();
+        }
+
+        public static void MakeAccount(ref Dictionary<string,string> user)
+        {
+            Console.Clear();
+            Console.Write("Please enter a username: ");
+            string userKey = Console.ReadLine();
+
+            // testing first to see if the username already exists
+            user.Add(userKey, "");
+            Console.Write("Please enter your password: ");
+            string userPass = Console.ReadLine();
+
+            // saving user input as the password assigned to the username key and printing the encrypted password
+            user[userKey] = userPass;
+            Console.WriteLine($"encrypted password: {Encrypt(userPass)}");
+            Console.ReadLine();
+        }
+
+        public static void Authenticate(Dictionary<string,string> user)
+        {
+            Console.Clear();
+            Console.WriteLine("Please enter a username");
+            string name = Console.ReadLine();
+
+            // first checking to see if the username exists or not
+            if (user.ContainsKey(name))
+            {
+                Console.Write($"Please enter the password for {name}: ");
+                string pass = Console.ReadLine();
+                
+                // if the username (key) exists next check to see if the password (value) is correct
+                if (user[name] == pass)
+                {
+                    Console.WriteLine("User Authenticated!");
+                }
+                else
+                {
+                    Console.WriteLine("Incorrect password");
+                }
+            }
+            else
+            {
+                Console.WriteLine("User does not exist");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
